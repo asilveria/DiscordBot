6bd4824d63ef6fb8ab7fde1a86d2175cbda894b1
asilveria <silveant@sonoma.edu>
2021-09-23 01:00:14 -0700

    Adding sqlite executables. First bot commands in place.

--- BotScriptMain.py
+++ BotScriptMain.py
@@ -1,11 +1,40 @@
 #Bot main.py
 import SQLdbconn
 import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
 
-def main():
-    # Testing function 
-    SQLdbconn.functionCallTest()
+#def main():
+    #SQLdbconn.insertHelper(dbconn, "tyler","09/22/2021", "I'll take my goop on the side")
+    #SQLdbconn.removeQuote(dbconn,3)
 
-    print("Hello test")
-    #Git Test 3:12 PM 9/21/21
-main()
+dbconn = SQLdbconn.fireUp()
+print("successful connection...")
+load_dotenv('ElmerInfo.env')
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+bot = discord.Client()
+print("client initialized...")
+
+# quote = SQLdbconn.returnQuoteBody(dbconn)
+# print(quote)
+
+#       0          1      2     3  
+#([Primary Key][author][date][body])
+bot = commands.Bot(command_prefix='!')
+
+@bot.command()
+async def quote(ctx):
+    quote = SQLdbconn.returnQuoteBody(dbconn)
+    await ctx.channel.send(quote[3])
+
+# Next commands should be allowing the bot to add a new quote to the DB
+    # It should snapshot the date added and have the author attached to the end of the command.
+    #  Ex) !addquote I'm tired of all this whataboutism - Brian
+    
+bot.run(TOKEN)
+
+
+#main()
--- SQLdbconn.py
+++ SQLdbconn.py
@@ -1,6 +1,7 @@
 
 from sqlite3.dbapi2 import sqlite_version
 import discord
+import random
 import sqlite3
 from sqlite3 import Error
 
@@ -20,23 +21,44 @@ def create_connection(db_file):
 
     return conn
 
-def create_table(conn, create_table_sql):
-    """ create a table from the create_table_sql statement
-    :param conn: Connection object
-    :param create_table_sql: a CREATE TABLE statement
-    :return:
-    """
-    try:
-        c = conn.cursor()
-        c.execute(create_table_sql)
-    except Error as e:
-        print(e)
+def insertHelper(conn, author, sub_date, quote_text):
+     quote = (author,sub_date,quote_text)
+     insertQuote(conn, quote)
 
-def functionCallTest():
-    print("Test Test Test")
-
-def tableCreate():
+def insertQuote(conn, quote):
     
+    sql = ''' INSERT INTO quotes (author, submit_date, quote_text)
+                VALUES(?,?,?)'''
+    cur = conn.cursor()
+    cur.execute(sql, quote)
+    conn.commit()
+
+    return cur.lastrowid
+
+def removeQuote(conn,id):
+
+    sql = '''DELETE FROM quotes WHERE id=?'''
+    cur = conn.cursor()
+    cur.execute(sql,(id,))
+    conn.commit()
+
+def returnQuoteBody(conn):
+    
+    #'''GO SELECT TOP 1 * FROM quotes ORDER BY NEWID() GO'''
+    quoteDeets = []
+    cursor = conn.execute('''SELECT * FROM quotes''')
+    record = cursor.fetchall()
+    for row in record:
+        quoteDeets.append(row[0])
+        quoteDeets.append(row[1])
+        quoteDeets.append(row[2])
+        quoteDeets.append(row[3])
+    
+    cursor.close()
+    return quoteDeets
+   
+def fireUp():
+
     database = "quotesqlite.db"
     sql_create_quotes_table = """ CREATE TABLE IF NOT EXISTS quotes (
                                         id integer PRIMARY KEY,
@@ -47,12 +69,4 @@ def tableCreate():
     # create a database connection
 
     conn = create_connection(database)
-
-    if conn is not None:
-    # create quotes table
-        create_table(conn, sql_create_quotes_table)
-
-    else:
-        print("Error! unable to establish database connection.")
-
-tableCreate()
+    return conn
